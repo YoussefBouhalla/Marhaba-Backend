fe1129e3276547329f5b84859fd24f40cba6c629
UssefBhl <ybhl83@gmail.com>
2022-04-02 19:04:29 +0000

    🚧 finished setting up Invoices controller

---
+++ src/controllers/Invoices.js
@@ -0,0 +1,17 @@
+const createInvoice = (req,res) => {}
+
+const sendInvoice = (req,res) => {}
+
+const getInvoices = (req,res) => {}
+
+const getInvoiceById = (req,res) => {}
+
+const deleteInvoice = (req,res) => {}
+
+module.exports = {
+    createInvoice,
+    sendInvoice,
+    getInvoices,
+    getInvoiceById,
+    deleteInvoice
+}
