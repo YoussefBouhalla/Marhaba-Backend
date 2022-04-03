f9a387f565b02cbc3b66c6eb596dfa4645973ced
UssefBhl <ybhl83@gmail.com>
2022-04-03 01:39:34 +0000

    🚧 finished setting up index for routes

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const AnnouncementsRoutes = require('./AnnouncementsRoutes')
+
+const ClientsRoutes = require('./ClientsRoutes')
+
+const CommandsRoutes = require('./CommandsRoutes')
+
+const DeliverersRoutes = require('./DeliverersRoutes')
+
+const InvoicesRoutes = require('./InvoicesRoutes')
+
+const MealsRoutes = require('./MealsRoutes')
+
+const UsersRoutes = require('./UsersRoutes')
+
+module.exports = {
+    AnnouncementsRoutes,
+    ClientsRoutes,
+    CommandsRoutes,
+    DeliverersRoutes,
+    InvoicesRoutes,
+    MealsRoutes,
+    UsersRoutes
+}
