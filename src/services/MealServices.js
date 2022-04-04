0f389f91bc041f02fe81a59d67a5743adbb1c64e
UssefBhl <ybhl83@gmail.com>
2022-04-04 12:10:34 +0000

    🚧 finished some meals services
    - getAll
    - getSingle
    - getCount
    - mealSearch

--- src/services/MealServices.js
+++ src/services/MealServices.js
@@ -13,6 +13,54 @@ const create = async (title, description, price, type) => {
     })
 }
 
+const getAll = async (type = 'none') => {
+    return await prisma.meals.findMany({
+        where: type === 'none' ? {} : {
+            type
+        }
+    });
+}
+
+const getSingle = async (id) => {
+    return await prisma.meals.findUnique({
+        where: {
+            meal_id: id
+        },
+    });
+}
+
+const getCount = async (type = 'none') => {
+    return await prisma.meals.aggregate({
+        where: type === 'none' ? {} : {
+            type
+        },
+        _count: {
+            meal_id: true
+        }
+    })
+}
+
+const mealSearch = async (options) => {
+    return await prisma.meals.findMany({
+        where: options !== {} ?{
+            AND: {
+                title: options.title ? {
+                    startsWith: options.title
+                } : {},
+                type : options.type ? options.type : {},
+                price: options.priceMax && options.priceMin ? {
+                    gte: options.priceMin,
+                    lte: options.priceMax
+                } : {}
+            }
+        } : {}
+    })
+}
+
 module.exports = {
-    create
+    create,
+    getAll,
+    getSingle,
+    getCount,
+    mealSearch
 }
