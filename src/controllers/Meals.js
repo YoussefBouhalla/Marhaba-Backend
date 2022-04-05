6f04175e0114b43b67a11a64f79d7cf644d32eb4
UssefBhl <ybhl83@gmail.com>
2022-04-05 10:15:09 +0000

    ♻️ cleared the meals controller

--- src/controllers/Meals.js
+++ src/controllers/Meals.js
@@ -1,31 +1,14 @@
-const {create, getAll, getSingle, getCount, mealSearch} = require('../services/MealServices')
+const createMeal = async (req,res) => {}
 
-const createMeal = async (req,res) => {
-    await create('Keto Cheesy Cabbage Sausage Skillet' , 'desczafozhaufoza', 5 , 'main');
-    res.status(200).json({message: 'Meal created!'})
-}
+const getMeals = async (req,res) => {}
 
-const getMeals = async (req,res) => {
-    let meals = await getAll('dessert');
-    res.status(200).json(meals)
-}
+const orderMeal = async (req,res) => {}
 
-const orderMeal = async (req,res) => {
-    let meal = await getSingle(parseInt(req.params.idMeal));
-    res.status(200).json(meal)
-}
-
-const searchForMeals = async(req,res) => {
-    let meals = await mealSearch({title: 'c' , type: 'dessert' , priceMin: 1 , priceMax: 4});
-    res.status(200).json(meals)
-}
+const searchForMeals = async(req,res) => {}
 
 const getRecommendedMeals = (req,res) => {}
 
-const getMealsCount = async(req,res) => {
-    let mealsCount = await getCount('dessert');
-    res.status(200).json(mealsCount)
-}
+const getMealsCount = async(req,res) => {}
 
 const updateMeal = (req,res) => {}
 
