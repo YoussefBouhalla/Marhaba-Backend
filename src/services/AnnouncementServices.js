1a9b263ce630d354b0a8981426427a7009e4fd31
UssefBhl <ybhl83@gmail.com>
2022-04-05 13:45:13 +0000

    🚧 finished some announcements services
    - getAll
    - getSingle
    - announcementSearch

--- src/services/AnnouncementServices.js
+++ src/services/AnnouncementServices.js
@@ -16,6 +16,59 @@ const create = async (title, description, price , meals_connections) => {
     });
 }
 
+const getAll = async () => {
+    return await prisma.announcements.findMany({
+        include: {
+            announcement_meal: {
+                include: {
+                    meals: true
+                }
+            }
+        }
+    });
+}
+
+const getSingle = async (id) => {
+    return await prisma.announcements.findUnique({
+        where : {
+            announcement_id : id    
+        },
+        include: {
+            announcement_meal: {
+                include: {
+                    meals: true
+                }
+            }
+        }
+    });
+}
+
+const announcementSearch = async (options) => {
+    return await prisma.announcements.findMany({
+        where: options ? {
+            AND: {
+                title: options.title ? {
+                    startsWith: options.title
+                } : {},
+                price: options.priceMax && options.priceMin ? {
+                    gte: options.priceMin,
+                    lte: options.priceMax
+                } : {}
+            }
+        } : {},
+        include: {
+            announcement_meal: {
+                include: {
+                    meals: true
+                }
+            }
+        }
+    })
+}
+
 module.exports = {
-    create
+    create,
+    getAll,
+    getSingle,
+    announcementSearch
 }
