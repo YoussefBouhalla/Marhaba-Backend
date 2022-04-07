4ddba9c628c59e25e3545ede57b6118de9b6d121
UssefBhl <ybhl83@gmail.com>
2022-04-07 15:17:36 +0000

    🚧 finished linking the services

---
+++ src/services/index.js
@@ -0,0 +1,13 @@
+const UserServices = require('./UserServices')
+const AnnouncementServices = require('./AnnouncementServices')
+const CommandServices = require('./CommandServices')
+const InvoiceServices = require('./InvoiceServices')
+const MealServices = require('./MealServices')
+
+module.exports = {
+    UserServices,
+    AnnouncementServices,
+    CommandServices,
+    InvoiceServices,
+    MealServices
+}
