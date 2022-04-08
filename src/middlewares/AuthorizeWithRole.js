b5358a3dfe533a2f8f903fc822521b97e8159fb6
UssefBhl <ybhl83@gmail.com>
2022-04-08 10:02:06 +0000

    🚧 finished the authorizeWith role middleware

---
+++ src/middlewares/AuthorizeWithRole.js
@@ -0,0 +1,11 @@
+const authorizeWithRole = (...roles) => {
+
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            res.status(401).json({ error: "you are not authorized" });
+        } else {
+            next()
+        }  
+    }
+
+}
