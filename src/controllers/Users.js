35d9277c06646ba75bbb7717735e4ddb42e070a9
UssefBhl <ybhl83@gmail.com>
2022-04-09 12:17:33 +0000

    🚧 finished some user controller methods
    - handleRegister
    - handleLogin

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -1,7 +1,43 @@
-const handleRegister = (req,res) => {}
+require('dotenv').config()
+const {JoiUtils} = require('../utils');
+const {UserServices} = require('../services');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const handleLogin = (req,res) => {
+const handleRegister = async (req,res) => {
+    try {
+        await UserServices.create(req.value);
+        res.status(200).json({meassage: "User created successfully!"});
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
+
+const handleLogin = async (req,res) => {
+    if(!req.body.email) throw res.status(400).json({error: "enter an email"});
+
+    try {
+        const user = await (await UserServices.getSingle({email: req.body.email}));
     
+        if(!user) {
+            res.status(400).json({error: "email doesn't exist"})
+        }else {
+            if (!req.body.password) {
+                res.status(400).json({error: "password is incorrect"});
+            }else {
+                bcrypt.compare(req.body.password,user.password).then(result => {
+                    if (!result) {
+                        res.status(400).json({error: "password is incorrect"});
+                    }else {
+                        const accessToken = jwt.sign({id: user.user_id, role: user.role} , process.env.ACCESS_TOKEN_SECRET);
+                        res.status(200).json({accessToken});
+                    }
+                })
+            }
+        }
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 const handleLogout = (req,res) => {}
