efd0bb0dfe8259856226688d84d303ddd652bbcb
UssefBhl <ybhl83@gmail.com>
2022-04-11 13:41:09 +0000

    🚧 finished announcements controller methods
    - createAnnouncement
    - getAnnouncements
    - searchForAnnouncements
    - getAnnouncementsCount
    - updateAnnouncement
    - deleteAnnouncement

--- src/controllers/Announcements.js
+++ src/controllers/Announcements.js
@@ -1,16 +1,94 @@
-const createAnnouncement = (req,res) => {}
+const {AnnouncementServices} = require('../services')
 
-const getAnnouncements = (req,res) => {}
+const createAnnouncement = async (req,res) => {
+    try {
+        await AnnouncementServices.create(req.value);
+        res.status(200).json({meassage: "Announcement created successfully!"});
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({error: error.message});
+    }
+}
+
+const getAnnouncements = async (req,res) => {
+    try {
+        let announcements = await AnnouncementServices.getAll();
+        res.status(200).json(announcements);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({error: error.message});
+    }
+}
 
 const orderAnnouncement = (req,res) => {}
 
-const searchForAnnouncements = (req,res) => {}
+const searchForAnnouncements = async (req,res) => {
+    const options = {
+        title : req.body.title,
+        priceMin: req.body.priceMin,
+        priceMax: req.body.priceMax
+    }
 
-const getAnnouncementsCount = (req,res) => {}
+    Object.keys(options).forEach(key => {
+        if (!options[key]) {
+            delete options[key]
+        }
+    });
 
-const updateAnnouncement = (req,res) => {}
+    try {
+        let announcements = await AnnouncementServices.announcementSearch();
+        res.status(200).json(announcements);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({error: error.message});
+    }
+}
 
-const deleteAnnouncement = (req,res) => {}
+const getAnnouncementsCount = async (req,res) => {
+    try {
+        let announcementsCount = await AnnouncementServices.getCount();
+        res.status(200).json({announcementsCount : announcementsCount._count.announcement_id});
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({error: error.message});
+    }
+}
+
+const updateAnnouncement = async (req,res) => {
+    const announcement_id = parseInt(req.params.idAnnouncement);
+    const options = {
+        title: req.body.title,
+        description: req.body.description,
+        price: parseInt(req.body.price),
+    }
+
+    Object.keys(options).forEach(key => {
+        if (!options[key]) {
+            delete options[key]
+        }
+    });
+
+    try {
+        if (Object.keys(options).length !== 0) {
+            await AnnouncementServices.updateA(announcement_id, options);
+            res.status(200).json({message: "Announcement updated successfully!"});
+        }else {
+            res.status(200).json({error: "please enter something to change"});
+        }
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
+
+const deleteAnnouncement = async (req,res) => {
+    const announcement_id = parseInt(req.params.idAnnouncement);
+    try {
+        await AnnouncementServices.deleteA(announcement_id);
+        res.status(200).json({message: "Announcement deleted successfully!"});
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
 
 module.exports = {
     createAnnouncement,
